da1b0b5adca6884046afe2027e39648522970b39
Henry Narzary <narzaryhenry0@gmail.com>
2025-04-26 18:10:43 +0530

    Route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+function Dashboard() {
+  return <h1 className="text-2xl font-bold p-4">Dashboard</h1>;
+}
+
+function KanbanBoard() {
+  return <h1 className="text-2xl font-bold p-4">Kanban Board</h1>;
+}
+
+function Analytics() {
+  return <h1 className="text-2xl font-bold p-4">Analytics</h1>;
+}
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/kanban" element={<KanbanBoard />} />
+          <Route path="/analytics" element={<Analytics />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
